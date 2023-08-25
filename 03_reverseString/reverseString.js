df542c2fb5fe618a7f04bacbd2fee363c41f8c45
DiptoSarkar182 <diptomanchester2018@gmail.com>
2023-08-25 16:50:50 +0600

    completed exercise 1 to 7

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const array = string.split("");
+    let reverse = array.reverse();
+    let join = reverse.join("");
+    return join;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(...args) {//([1, 2, 3, 4],3, 2)
+    let array = args[0];//[1,2,3,4]
+    let array2 = [];//3,2
+    for (let i = 1;i<args.length;i++){
+        array2.push(args[i]);
+    }
+    for (let i = array.length - 1; i >= 0; i--) {
+        for (let j = 0; j < array2.length; j++) {
+            if (array2[j] === array[i]) {
+                array.splice(i, 1);
+                break; 
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
